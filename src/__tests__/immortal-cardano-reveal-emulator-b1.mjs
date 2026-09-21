d73f39199ae53e79e415daa68d91fe2cd92a62c2
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-09-21 10:12:27 +0200

    test: serialize P2.8-B.1 reveal witnesses with Data.to

--- src/__tests__/immortal-cardano-reveal-emulator-b1.mjs
+++ src/__tests__/immortal-cardano-reveal-emulator-b1.mjs
@@ -888,14 +888,16 @@ async function main() {
       .collectFrom(
         [prizeUtxos[0]],
 
-        constr(
-          1,
+        Data.to(
+          constr(
+            1,
 
-          [
-            bytesData(
-              toHex(playerSecret),
-            ),
-          ],
+            [
+              bytesData(
+                toHex(playerSecret),
+              ),
+            ],
+          ),
         ),
       )
 
@@ -906,14 +908,16 @@ async function main() {
       .collectFrom(
         [poolUtxos[0]],
 
-        constr(
-          2,
+        Data.to(
+          constr(
+            2,
 
-          [
-            BigInt(
-              priceUsdm,
-            ),
-          ],
+            [
+              BigInt(
+                priceUsdm,
+              ),
+            ],
+          ),
         ),
       )
 
