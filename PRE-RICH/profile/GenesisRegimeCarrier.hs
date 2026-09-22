f87b59b5c8159013b8543e340e99c54e4bb9fbe0
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-09-22 21:50:37 +0200

    fix: make Genesis carrier validator self-contained and authenticated

--- PRE-RICH/profile/GenesisRegimeCarrier.hs
+++ PRE-RICH/profile/GenesisRegimeCarrier.hs
@@ -24,21 +24,32 @@ import Types
   ( OracleDatum (..)
   , OracleStateId (..)
   , TreasuryDatum
+  , precision
   )
 
+-- | Application-owned singleton lifecycle carrier.
+-- The carrier is deliberately separate from Treasury and B1PrizePool.
 data PreRichRegimeDatum = PreRichRegimeDatum
-  { prdRegime            :: Integer
-  , prdTransitionNonce   :: Integer
-  , prdCanonicalTreasury :: ScriptHash
-  , prdPrePolicy         :: BuiltinByteString
-  , prdPreAssetName      :: BuiltinByteString
-  , prdStateVersion      :: Integer
+  { prdRegime             :: Integer
+  , prdTransitionNonce    :: Integer
+  , prdCanonicalTreasury  :: ScriptHash
+  , prdPrePolicy          :: BuiltinByteString
+  , prdPreAssetName       :: BuiltinByteString
+  , prdOraclePolicy       :: BuiltinByteString
+  , prdOracleName         :: BuiltinByteString
+  , prdOraclePublisher    :: PubKeyHash
+  , prdCarrierPolicy      :: BuiltinByteString
+  , prdCarrierName        :: BuiltinByteString
+  , prdPrizePoolHash      :: ScriptHash
+  , prdStateVersion       :: Integer
   }
 
 PlutusTx.unstableMakeIsData ''PreRichRegimeDatum
 
 data GenesisRegimeAction
   = ActivateGenesis
+
+PlutusTx.unstableMakeIsData ''GenesisRegimeAction
 
 PlutusTx.unstableMakeIsData ''GenesisRegimeAction
 
@@ -65,12 +76,13 @@ ownScriptHash ctx =
 {-# INLINABLE countOwnInputs #-}
 countOwnInputs :: ScriptContext -> Integer
 countOwnInputs ctx =
-  let sh = ownScriptHash ctx
-      go [] = 0
-      go (i:is) =
-        case addressCredential (txOutAddress (txInInfoResolved i)) of
-          ScriptCredential h | h == sh -> 1 + go is
-          _ -> go is
+  let
+    sh = ownScriptHash ctx
+    go [] = 0
+    go (i:is) =
+      case addressCredential (txOutAddress (txInInfoResolved i)) of
+        ScriptCredential h | h == sh -> 1 + go is
+        _ -> go is
   in go (txInfoInputs (scriptContextTxInfo ctx))
 
 {-# INLINABLE decodeRegimeDatum #-}
@@ -85,7 +97,7 @@ decodeRegimeDatum info out =
     NoOutputDatum -> Nothing
 
 {-# INLINABLE findSingleOwnOutput #-}
-findSingleOwnOutput :: ScriptContext -> Maybe PreRichRegimeDatum
+findSingleOwnOutput :: ScriptContext -> Maybe (TxOut, PreRichRegimeDatum)
 findSingleOwnOutput ctx =
   let
     info = scriptContextTxInfo ctx
@@ -98,15 +110,64 @@ findSingleOwnOutput ctx =
             Just _ -> Nothing
             Nothing ->
               case decodeRegimeDatum info o of
-                Just d -> go os (Just d)
+                Just d -> go os (Just (o, d))
                 Nothing -> Nothing
         _ -> go os found
   in go (txInfoOutputs info) Nothing
 
-{-# INLINABLE singletonTokenAmount #-}
-singletonTokenAmount :: Value -> BuiltinByteString -> BuiltinByteString -> Integer
-singletonTokenAmount val policy name =
+{-# INLINABLE singletonAmount #-}
+singletonAmount :: Value -> BuiltinByteString -> BuiltinByteString -> Integer
+singletonAmount val policy name =
   valueOf val (CurrencySymbol policy) (TokenName name)
+
+{-# INLINABLE totalTokenAmountInputs #-}
+totalTokenAmountInputs :: TxInfo -> BuiltinByteString -> BuiltinByteString -> Integer
+totalTokenAmountInputs info policy name =
+  go (txInfoInputs info)
+  where
+    go [] = 0
+    go (i:is) =
+      singletonAmount
+        (txOutValue (txInInfoResolved i))
+        policy
+        name
+      + go is
+
+{-# INLINABLE totalTokenAmountOutputs #-}
+totalTokenAmountOutputs :: TxInfo -> BuiltinByteString -> BuiltinByteString -> Integer
+totalTokenAmountOutputs info policy name =
+  go (txInfoOutputs info)
+  where
+    go [] = 0
+    go (o:os) =
+      singletonAmount (txOutValue o) policy name + go os
+
+{-# INLINABLE anyScriptInput #-}
+anyScriptInput :: ScriptHash -> [TxInInfo] -> Bool
+anyScriptInput _ [] = False
+anyScriptInput sh (i:is) =
+  case addressCredential (txOutAddress (txInInfoResolved i)) of
+    ScriptCredential h -> h == sh || anyScriptInput sh is
+    _ -> anyScriptInput sh is
+
+{-# INLINABLE anyScriptOutput #-}
+anyScriptOutput :: ScriptHash -> [TxOut] -> Bool
+anyScriptOutput _ [] = False
+anyScriptOutput sh (o:os) =
+  case addressCredential (txOutAddress o) of
+    ScriptCredential h -> h == sh || anyScriptOutput sh os
+    _ -> anyScriptOutput sh os
+
+{-# INLINABLE decodeTreasuryDatum #-}
+decodeTreasuryDatum :: TxInfo -> TxOut -> Maybe TreasuryDatum
+decodeTreasuryDatum info out =
+  case txOutDatum out of
+    OutputDatum d -> fromBuiltinData (getDatum d)
+    OutputDatumHash dh ->
+      case findDatum dh info of
+        Just d -> fromBuiltinData (getDatum d)
+        Nothing -> Nothing
+    NoOutputDatum -> Nothing
 
 {-# INLINABLE findSingleTreasuryReference #-}
 findSingleTreasuryReference :: ScriptHash -> TxInfo -> Maybe TxOut
@@ -116,18 +177,19 @@ findSingleTreasuryReference treasuryHash info =
     go [] found = found
     go (i:is) found =
       case addressCredential (txOutAddress (txInInfoResolved i)) of
-        ScriptCredential h | h == treasuryHash ->
-          case found of
-            Just _ -> Nothing
-            Nothing ->
-              case decodeTreasuryDatum info (txInInfoResolved i) of
-                Just _ -> go is (Just (txInInfoResolved i))
-                Nothing -> Nothing
+        ScriptCredential h
+          | h == treasuryHash ->
+              case found of
+                Just _ -> Nothing
+                Nothing ->
+                  case decodeTreasuryDatum info (txInInfoResolved i) of
+                    Just _ -> go is (Just (txInInfoResolved i))
+                    Nothing -> Nothing
         _ -> go is found
 
-{-# INLINABLE decodeTreasuryDatum #-}
-decodeTreasuryDatum :: TxInfo -> TxOut -> Maybe TreasuryDatum
-decodeTreasuryDatum info out =
+{-# INLINABLE decodeOracleDatum #-}
+decodeOracleDatum :: TxInfo -> TxOut -> Maybe OracleDatum
+decodeOracleDatum info out =
   case txOutDatum out of
     OutputDatum d -> fromBuiltinData (getDatum d)
     OutputDatumHash dh ->
@@ -151,7 +213,7 @@ findSingleOracleReference stateId assetPolicy assetName publisher info =
     go (i:is) found =
       let out = txInInfoResolved i
           singletonOk =
-            singletonTokenAmount
+            singletonAmount
               (txOutValue out)
               (osiPolicy stateId)
               (osiName stateId) == 1
@@ -169,58 +231,40 @@ findSingleOracleReference stateId assetPolicy assetName publisher info =
               Nothing -> go is (Just od)
         _ -> go is found
 
-{-# INLINABLE decodeOracleDatum #-}
-decodeOracleDatum :: TxInfo -> TxOut -> Maybe OracleDatum
-decodeOracleDatum info out =
-  case txOutDatum out of
-    OutputDatum d -> fromBuiltinData (getDatum d)
-    OutputDatumHash dh ->
-      case findDatum dh info of
-        Just d -> fromBuiltinData (getDatum d)
-        Nothing -> Nothing
-    NoOutputDatum -> Nothing
-
-{-# INLINABLE treasuryObservation #-}
-treasuryObservation
+{-# INLINABLE authenticatedObservation #-}
+authenticatedObservation
   :: PreRichRegimeDatum
-  -> ScriptHash
-  -> OracleStateId
-  -> PubKeyHash
   -> TxInfo
   -> Maybe GenesisTreasuryObservation
-treasuryObservation datum treasuryHash oracleState publisher info =
-  case findSingleTreasuryReference treasuryHash info of
+authenticatedObservation datum info =
+  case findSingleTreasuryReference (prdCanonicalTreasury datum) info of
     Nothing -> Nothing
     Just treasury ->
       case findSingleOracleReference
-             oracleState
+             (OracleStateId (prdOraclePolicy datum) (prdOracleName datum))
              (prdPrePolicy datum)
              (prdPreAssetName datum)
-             publisher
+             (prdOraclePublisher datum)
              info of
         Nothing -> Nothing
         Just oracle ->
-          Just GenesisTreasuryObservation
-            { gtoTreasuryIdentityVerified =
-                treasuryHash == prdCanonicalTreasury datum
-            , gtoSourceRegimePreGenesis =
-                prdRegime datum == preGenesisTag
-            , gtoPreAssetVerified =
-                singletonTokenAmount
+          let quantity =
+                singletonAmount
                   (txOutValue treasury)
                   (prdPrePolicy datum)
-                  (prdPreAssetName datum) >= 0
+                  (prdPreAssetName datum)
+          in
+          Just GenesisTreasuryObservation
+            { gtoTreasuryIdentityVerified = True
+            , gtoSourceRegimePreGenesis = prdRegime datum == preGenesisTag
+            , gtoPreAssetVerified = quantity >= 0
             , gtoTreasuryStateVerified = True
             , gtoOracleVerified = True
             , gtoOracleFresh =
                 validOracleTimestamp (odTimestamp oracle) info
-            , gtoPreQuantity =
-                singletonTokenAmount
-                  (txOutValue treasury)
-                  (prdPrePolicy datum)
-                  (prdPreAssetName datum)
+            , gtoPreQuantity = quantity
             , gtoVerifiedPreUsdmPrice = odPrice oracle
-            , gtoOraclePrecision = 100
+            , gtoOraclePrecision = precision
             }
 
 {-# INLINABLE outputMatchesGenesis #-}
@@ -233,76 +277,63 @@ outputMatchesGenesis before after =
   && prdCanonicalTreasury after == prdCanonicalTreasury before
   && prdPrePolicy after == prdPrePolicy before
   && prdPreAssetName after == prdPreAssetName before
+  && prdOraclePolicy after == prdOraclePolicy before
+  && prdOracleName after == prdOracleName before
+  && prdOraclePublisher after == prdOraclePublisher before
+  && prdCarrierPolicy after == prdCarrierPolicy before
+  && prdCarrierName after == prdCarrierName before
+  && prdPrizePoolHash after == prdPrizePoolHash before
 
 {-# INLINABLE mkValidator #-}
-mkValidator
-  :: BuiltinByteString
-  -> BuiltinByteString
-  -> BuiltinByteString
-  -> PubKeyHash
-  -> ScriptContext
-  -> Bool
-mkValidator
-  poolPolicy
-  poolName
-  oraclePolicy
-  oraclePublisher
-  ctx =
-  let
-    info = scriptContextTxInfo ctx
-    before =
-      case decodeRegimeDatum info (ownInputResolved ctx) of
-        Just d -> d
-        Nothing -> traceError "GenesisRegimeCarrier: invalid input datum"
-    action =
-      case txInfoRedeemer info of
-        _ -> ActivateGenesis
-    after =
-      case findSingleOwnOutput ctx of
-        Just d -> d
-        Nothing -> traceError "GenesisRegimeCarrier: invalid continuing output"
-    oracleState =
-      OracleStateId oraclePolicy "ORACLE"
-    observation =
-      treasuryObservation
-        before
-        (prdCanonicalTreasury before)
-        oracleState
-        oraclePublisher
-        info
-  in
-       countOwnInputs ctx == 1
-    && prdRegime before == preGenesisTag
-    && outputMatchesGenesis before after
-    && case observation of
-         Nothing -> False
-         Just o -> genesisPredicate o
-    && singletonTokenAmount
-         (txOutValue (ownInputResolved ctx))
-         poolPolicy
-         poolName == 1
-    && singletonTokenAmount
-         (txOutValue (ownInputOutput ctx))
-         poolPolicy
-         poolName == 1
-
-{-# INLINABLE ownInputOutput #-}
-ownInputOutput :: ScriptContext -> TxOut
-ownInputOutput ctx =
-  case findOwnInput ctx of
-    Just i -> txInInfoResolved i
-    Nothing -> traceError "GenesisRegimeCarrier: missing own input"
+mkValidator :: PreRichRegimeDatum -> GenesisRegimeAction -> ScriptContext -> Bool
+mkValidator datum action ctx =
+  case action of
+    ActivateGenesis ->
+      let
+        info = scriptContextTxInfo ctx
+        before = datum
+        after =
+          case findSingleOwnOutput ctx of
+            Just (_, d) -> d
+            Nothing -> traceError "GenesisRegimeCarrier: expected one continuing output"
+        ownIn =
+          ownInputResolved ctx
+        ownOut =
+          case findSingleOwnOutput ctx of
+            Just (o, _) -> o
+            Nothing -> traceError "GenesisRegimeCarrier: missing output"
+        observation = authenticatedObservation before info
+        carrierInputs =
+          totalTokenAmountInputs info (prdCarrierPolicy before) (prdCarrierName before)
+        carrierOutputs =
+          totalTokenAmountOutputs info (prdCarrierPolicy before) (prdCarrierName before)
+      in
+           countOwnInputs ctx == 1
+        && outputMatchesGenesis before after
+        && carrierInputs == 1
+        && carrierOutputs == 1
+        && singletonAmount
+             (txOutValue ownIn)
+             (prdCarrierPolicy before)
+             (prdCarrierName before) == 1
+        && singletonAmount
+             (txOutValue ownOut)
+             (prdCarrierPolicy before)
+             (prdCarrierName before) == 1
+        && not (anyScriptInput (prdPrizePoolHash before) (txInfoInputs info))
+        && not (anyScriptOutput (prdPrizePoolHash before) (txInfoOutputs info))
+        && case observation of
+             Nothing -> False
+             Just o -> genesisPredicate o
 
 {-# INLINABLE wrap #-}
 wrap :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinUnit
-wrap d _ ctx =
+wrap d r ctx =
   check
     (mkValidator
       (unsafeFromBuiltinData d)
-      emptyByteString
-      emptyByteString
-      (unsafeFromBuiltinData ctx)
-      )
+      (unsafeFromBuiltinData r)
+      (unsafeFromBuiltinData ctx))
 
 compiledValidator :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinUnit)
 compiledValidator = $$(compile [|| wrap ||])
