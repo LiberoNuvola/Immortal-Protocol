c7cca34dbdd9411cb1c3f18db603c1701d64bf6e
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-09-22 09:50:47 +0200

    Fix governance commitment hex text decoding

--- IMMORTAL/governance/GovernanceCommitment.hs
+++ IMMORTAL/governance/GovernanceCommitment.hs
@@ -2,7 +2,7 @@ module GovernanceCommitment
   ( commitmentAlgorithm, commitmentDigestHex, commitmentMatches ) where
 
 import qualified Crypto.Hash.SHA256 as SHA256
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.Text
 import qualified Data.Text.Encoding as TE
@@ -14,7 +14,7 @@ commitmentAlgorithm = "SHA-256 over UTF-8 canonical event representation"
 
 commitmentDigestHex :: CanonicalEvent -> String
 commitmentDigestHex e =
-  map lower (BS.unpack (B16.encode (SHA256.hash (TE.encodeUtf8 (Data.Text.pack (canonicalEventBytes e))))))
+  map lower (BSC.unpack (B16.encode (SHA256.hash (TE.encodeUtf8 (Data.Text.pack (canonicalEventBytes e))))))
   where
     lower w | w >= 65 && w <= 70 = w + 32
             | otherwise = w
