e524ebc83a027c2914a22588007337adeca5ca16
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-09-24 20:33:33 +0200

    fix: decode Babbage Tx through native annotator

--- audit/cardano-ledger-runner/TypedPacketDecode.hs
+++ audit/cardano-ledger-runner/TypedPacketDecode.hs
@@ -8,11 +8,13 @@ module TypedPacketDecode
 
 import Cardano.Ledger.Api (BabbageEra, PParams, Tx)
 import Cardano.Ledger.Api.PParams (ppProtocolVersionL)
-import Cardano.Ledger.Binary.Decoding (decodeFull')
+import Cardano.Ledger.Binary.Decoding (decodeFullAnnotator)
 import Cardano.Ledger.Binary.Version (Version, mkVersion)
 import Cardano.Ledger.Core (TopTx, pvMajor)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import Cardano.Ledger.Binary.Decoding.DecCBOR (decCBOR)
 import Lens.Micro ((^.))
 
 decodeBabbagePParams :: BS.ByteString -> Either String (PParams BabbageEra)
@@ -21,7 +23,7 @@ decodeBabbagePParams = Aeson.eitherDecodeStrict'
 decodeBabbageTx :: PParams BabbageEra -> BS.ByteString -> Either String (Tx TopTx BabbageEra)
 decodeBabbageTx pp bytes = do
   version <- protocolVersionToBinaryVersion pp
-  case decodeFull' version bytes of
+  case decodeFullAnnotator version "Babbage Tx" decCBOR (BSL.fromStrict bytes) of
     Left err -> Left (show err)
     Right tx -> Right tx
 
