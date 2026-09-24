ddec34d8d3cf5ec0de2872096cf8d22d130333d6
LiberoNuvola <Edicoladannunzio@gmail.com>
2026-09-24 06:53:28 +0200

    GOV-28 map finalized decision to Accepted projection

--- IMMORTAL/governance/GovernanceCanonicalReplay.hs
+++ IMMORTAL/governance/GovernanceCanonicalReplay.hs
@@ -54,7 +54,7 @@ applyDecisionFinalized st r at = do
        else if not (finalizationReady p (decisionChallenges r) at)
          then Left "decision finalization prerequisites not satisfied"
          else Right st { proposals = [ if proposalId p' == proposalId p
-                                      then p' { finalizationAt = Just at }
+                                      then p' { finalizationAt = Just at, proposalStatus = Accepted }
                                       else p'
                                     | p' <- proposals st ]
                        , eventsApplied = eventsApplied st + 1
